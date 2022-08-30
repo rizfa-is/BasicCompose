c65137ffeaeabee705dff03c86018d0e53c8363c
Rosyid Rosadi <rosyidrosadi15@gmail.com>
2022-08-30 23:13:40 +0700

    customize show lees / show more button add dummy content

--- app/src/main/java/com/example/basiccodelabcompose/MainActivity.kt
+++ app/src/main/java/com/example/basiccodelabcompose/MainActivity.kt
@@ -8,13 +8,14 @@ import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedButton
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -61,10 +62,22 @@ fun Greeting(name: String) {
                         fontWeight = FontWeight.ExtraBold
                     )
                 )
+                if (isExpanded.value) {
+                    Text(text = ("Composem ipsum color sit lazy, " +
+                            "padding theme elit, sed do bouncy. ").repeat(4)
+                    )
+                }
             }
 
-            OutlinedButton(onClick = { isExpanded.value = !isExpanded.value }) {
-                Text(if (isExpanded.value) "Show less" else "Show more")
+            IconButton(onClick = { isExpanded.value = !isExpanded.value }) {
+                Icon(
+                    imageVector = if (isExpanded.value) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                    contentDescription = if (isExpanded.value) {
+                        stringResource(R.string.show_less)
+                    } else {
+                        stringResource(R.string.show_more)
+                    }
+                )
             }
         }
     }
